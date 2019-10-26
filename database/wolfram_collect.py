33e6139acb4d0a6e31c96ca1d74ebd2affcca848
Anthony <anthony@hevia.dev>
2019-10-25 22:46:04 -0400

    added some stuff

--- database/wolfram_collect.py
+++ database/wolfram_collect.py
@@ -1,3 +1,7 @@
 from wolframclient.evaluation import WolframLanguageSession
 from wolframclient.language import wl, wlexpr
 session = WolframLanguageSession()
+
+session.evaluate(Interpreter[
+Restricted["Animal",
+EntityClass["Species", "EndangeredSpecies"]]]["seahorse"])
