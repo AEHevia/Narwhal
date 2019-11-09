f84d7eee6bf17c2cba1383911c15781729168fb9
Anthony <anthony@hevia.dev>
2019-11-09 17:03:41 -0500

    modified some stuffaroo

--- database/scripts/data_clean.py
+++ database/scripts/data_clean.py
@@ -1,2 +1,14 @@
 from pickle_jar import savePickles, loadPickles
 import spacy
+
+def file_open(file_name):
+    f = open(file_name, 'r')
+    data = f.read().split("\n")
+    return_list = []
+    for i in data:
+        return_list.append(i)
+    return return_list
+
+
+animal_list = file_open('../constants/animals.txt')
+# animal_dict = loadPickles('animal_dict')
